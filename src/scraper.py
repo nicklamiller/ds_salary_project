492354d34bf69fde5c9168ea65626954a8d41ace
nicklamiller <nicklamiller@gmail.com>
2023-06-02 21:08:09 -0700

    fix linting issues

--- src/scraper.py
+++ src/scraper.py
@@ -4,6 +4,7 @@ from typing import List
 import bs4
 import pandas as pd
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 from src.base_models import StrictBaseModel
 from src.configuration import JobInfoField, JobInfoFields, ScrapingConfig
@@ -17,7 +18,8 @@ class WebScraper(StrictBaseModel):
     def scrape_data(self) -> pd.DataFrame:
         response = self.fetch_url_data()
         all_jobs = self.find_all_jobs(response)
-        return self.extract_all_jobs_info(all_jobs)
+        extracted_jobs = self.extract_all_jobs_info(all_jobs)
+        return pd.DataFrame(extracted_jobs)
 
     def fetch_url_data(self) -> str:
         driver = webdriver.Firefox()
@@ -46,32 +48,6 @@ class WebScraper(StrictBaseModel):
         return all_jobs_info
 
 
-# def _extract_single_job_info(job):
-#     opening = {}
-#     try:
-#         opening['name-of-company'] = job.find('div',
-#                                               {'class': 'd-flex align-items-center'}).text
-#     except:
-#         opening['name-of-company'] = None
-
-#     try:
-#         opening['name-of-job'] = job.find('div',
-#                                           {'class': 'job-title mt-xsm'}).text
-#     except:
-#         opening['name-of-job'] = None
-
-#     try:
-#         opening['location'] = job.find(
-#             'div', {'class': 'location mt-xxsm'}).text
-#     except:
-#         opening['location'] = None
-
-#     try:
-#         opening['salary'] = job.find('div', {'class': 'salary-estimate'}).text
-#     except:
-#         opening['salary'] = None
-
-
 def _extract_job_all_info_fields(
     single_job_info: bs4.element.Tag,
 ) -> dict:
@@ -95,53 +71,13 @@ def _extract_job_info_field(
         opening[job_info_field.dict_keyword] = single_job_info.find(
             'div',
             {'class': job_info_field.div_class_pattern},
-        )
-    except:
+        ).text
+    except NoSuchElementException:
         opening[job_info_field.dict_keyword] = None
     return opening
 
 
 if __name__ == '__main__':
     web_scraper = WebScraper()
-    scraped_data = web_scraper.fetch_url_data()
-
-
-# all_openings = []
-
-
-# soup = BeautifulSoup(response, 'html.parser')
-# all_jobs_container = soup.find('ul', {'class': 'css-7ry9k1'})
-# all_jobs = all_jobs_container.find_all('li')
-# for job in all_jobs:
-
-#     opening = {}
-    # try:
-    #     opening['name-of-company'] = job.find('div',
-    #                                           {'class': 'd-flex align-items-center'}).text
-    # except:
-    #     opening['name-of-company'] = None
-
-    # try:
-    #     opening['name-of-job'] = job.find('div',
-    #                                       {'class': 'job-title mt-xsm'}).text
-    # except:
-    #     opening['name-of-job'] = None
-
-    # try:
-    #     opening['location'] = job.find(
-    #         'div', {'class': 'location mt-xxsm'}).text
-    # except:
-    #     opening['location'] = None
-
-    # try:
-    #     opening['salary'] = job.find('div', {'class': 'salary-estimate'}).text
-    # except:
-    #     opening['salary'] = None
-
-#     all_openings.append(opening)
-#     opening = {}
-
-# print(all_openings)
-# df = pd.DataFrame(all_openings)
-# print(df.head())
-# print(df.shape)
+    scraped_data = web_scraper.scrape_data()
+    scraped_data.head()
